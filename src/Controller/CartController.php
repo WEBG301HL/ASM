c17c1f0a9bdea3146c029110df3070f857c77de6
MinhGCC200321 <minhtqgcc200321@fpt.edu.vn>
2023-02-28 10:55:55 +0700

    add route cart and show cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,19 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\CartRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    /**
+     /**
      * @Route("/cart", name="app_cart")
      */
-    public function index(): Response
+    public function showCart(CartRepository $repo): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        $user = $this->getUser();
+        if ($user != "") {
+            $carts = $repo->cartShow($user->getId());
+            return $this->render('cart/index.html.twig',[
+                'carts'=>$carts
+            ]);   
+        }
+        else{
+            return $this->redirectToRoute('homepage');
+        }
     }
+
 }
